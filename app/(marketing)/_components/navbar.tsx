4cbc575baad1042aee6dee9f8217a06255f0eb8c
shekhar389 <kshekhar2807@gmail.com>
2023-10-19 17:34:51 +0530

    Completed SideBar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -40,6 +40,7 @@ export const Navbar = () => {
                     <UserButton afterSignOutUrl="/"/>
                     </>
                 )}
+                
                 <ModeToggle/>
             </div>
         </div>
