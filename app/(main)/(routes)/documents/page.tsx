41eedd3bbd912bdf3a9315edb5cd2519ee3fab9b
shekhar389 <kshekhar2807@gmail.com>
2023-10-20 15:50:00 +0530

    added Userdetails in navbar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useUser } from "@clerk/clerk-react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const {user} = useUser();
     return (
-        <div>
-        This is a page
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+        <Image src="/empty.png" width="300" height="300" alt="Empty" className="dark:hidden"/>
+        <Image src="/empty.png" width="300" height="300" alt="Empty" className="hidden dark:block"/>
+        <h2 className="text-lg font-medium"
+        >Welcome to {user?.firstName}&apos;s Notion
+        </h2>
+        <Button>
+            <PlusCircle className="h-4 w-4 mr-2"/>
+            New Document
+        </Button>
         </div>
     );
     };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { UserItem } from "./user-item";
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
@@ -107,7 +108,7 @@ export const Navigation = () => {
             <ChevronsLeft className="h-6 w-6"/>
         </div>
         <div>
-            <p>Action Items</p>
+            <UserItem/>
         </div>
         <div className="mt-4">
             <p>Documents</p>
